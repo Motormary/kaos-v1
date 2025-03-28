56711f4bff4ad06f30a23346271a42541b9b1560
Mathias <mat.moen@gmail.com>
2025-03-28 14:40:29 +0100

    fix: remove logs

--- lib/cookieStore.ts
+++ lib/cookieStore.ts
@@ -4,8 +4,6 @@ import { CookieOptions } from "@supabase/ssr"
 import { cookies } from "next/headers"
 
 export async function setCookie(key: string, value: string, options: CookieOptions) {
-  console.log("🚀 ~ setCookie ~ value:", value)
-  console.log("🚀 ~ setCookie ~ key:", key)
   const cookieStore = await cookies()
   cookieStore.set(key, value, options)
 }
